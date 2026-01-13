962f832b978c78cb9fc8d00effe1aab1c3504132
ThiagoSantos7 <seu-email>
2026-01-13 19:41:58 -0300

    Projeto para empresa THP Solutions

---
+++ php/cod.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&display=swap"
+        rel="stylesheet">
+    <title>Orçamento</title>
+</head>
+
+<body>
+    <header>
+        <div>
+            <img src="../assets/logo3.svg" alt="">
+        </div>
+        <h1>THP Solution <sup><small>©</small></sup></h1>
+    </header>
+
+    <main>
+        <?php
+        $nome = isset($_GET["nome"]) ? $_GET["nome"] : "";
+        $idade = isset($_GET["idade"]) ? $_GET["idade"] : 16;
+        ?>
+        <fieldset>
+            <legend>Olá, <?= $nome ?>! Agende já seu Orçamento!</legend>
+            <form>
+                <label for="empresa">Qual seu ramo?</label>
+                <select name="empresa" id="idempresa">
+                    <option value="" disabled>Selecione</option>
+                    <option value="1">Empresa</option>
+                    <option value="2">Autônomo</option>
+                    <option value="3">Pessoal</option>
+                </select>
+                <label for="site">Qual tipo de serviço deseja?</label>
+                <select name="site" id="idsite">
+                    <option value="" disabled>Selecione</option>
+                    <option value="1">Site e-commerce</option>
+                    <option value="2">Landing Page</option>
+                    <option value="3">Link Bio</option>
+                    <option value="4">Midias sociais</option>
+                </select>
+                <label for="tipoEmpresa">Descreva qual o tipo de empresa:</label>
+                <input type="text" name="tipoEmpresa" placeholder="Ex: Ramo de advocacia autônomo...">
+                <label for="dataAg">Data para contato:</label>
+                <input type="date" name="dataAg">
+                <label for="horario">Qaul melhor horário?</label>
+                <input type="time" name="horario">
+                <label for="textoFinal">Agora detalhe seu tipo de serviço:</label>
+                <textarea name="textoFinal"
+                    placeholder="Ex: Site institucional com foco na experiência do usuário, moderno etc."></textarea>
+            </form>
+        </fieldset>
+    </main>
+</body>
+
+</html>
